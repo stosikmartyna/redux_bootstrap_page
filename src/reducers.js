9e62eee6d797e2dca6522b5c8f57f39edd1b2bb0
stosikmartyna <stosik.martyna@gmail.com>
2020-08-21 14:08:54 +0200

    Add counter based on Redux.

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import plantsReducer from './app/plants/plants_reducers';
 import flowerpotsReducer from './app/flowerpots/flowerpots_index';
+import counterReducer from './reducers/counter';
 
 const rootReducer = combineReducers({
     plants: plantsReducer,
-    flowerpots: flowerpotsReducer
+    flowerpots: flowerpotsReducer,
+    counter: counterReducer
 })
 
 export default rootReducer
