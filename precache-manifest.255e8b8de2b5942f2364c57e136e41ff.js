ac7839ab2316a8220f5debaae41f9c42cf38ee22
stosikmartyna <stosik.martyna@gmail.com>
2020-08-30 14:05:11 +0200

    Updates

---
+++ precache-manifest.255e8b8de2b5942f2364c57e136e41ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08883000916bcfef4f66e8405dc271f1",
+    "url": "/redux_bootstrap_page/index.html"
+  },
+  {
+    "revision": "54212ba18c6240db0f94",
+    "url": "/redux_bootstrap_page/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b632f2e4d82a2c132a1a",
+    "url": "/redux_bootstrap_page/static/css/main.c1efb092.chunk.css"
+  },
+  {
+    "revision": "54212ba18c6240db0f94",
+    "url": "/redux_bootstrap_page/static/js/2.442e1457.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/redux_bootstrap_page/static/js/2.442e1457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b632f2e4d82a2c132a1a",
+    "url": "/redux_bootstrap_page/static/js/main.f865b844.chunk.js"
+  },
+  {
+    "revision": "2a8b352c121776bc7b45",
+    "url": "/redux_bootstrap_page/static/js/runtime-main.bb24b6ee.js"
+  }
+]);
